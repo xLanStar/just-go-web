389c6f69ada6b2fb690ea032733138265ee450e5
flystar0526 <flowermetoer@gmail.com>
2024-06-19 21:24:58 +0800

    add signin page rwd

--- src/pages/auth/Signin.tsx
+++ src/pages/auth/Signin.tsx
@@ -18,6 +18,8 @@ import {
 import { User, setUser } from "../../feature/user/userSlice";
 import { useAppDispatch } from "../../hooks";
 
+import "../../assets/scss/signin.scss";
+
 export interface LoginFormData {
   name?: string;
   email: string;
@@ -69,108 +71,117 @@ const Signin: React.FunctionComponent = () => {
   return (
     <Fragment>
       {contextHolder}
-      <Flex vertical justify="center" align="center" style={{ height: "100%" }}>
-        <img
-          src="src/assets/logo.png"
-          alt="logo.png"
-          width={480}
-          height={195}
-        />
-        <h2>{isSignin ? "登入" : "註冊"}</h2>
-        <Form
-          form={form}
-          layout="vertical"
-          scrollToFirstError
-          onFinish={onFinish}
-          noValidate
-          style={{
-            width: "480px",
-          }}
-        >
-          {!isSignin && (
+      <Flex
+        vertical
+        justify="center"
+        align="center"
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <Flex className="signin_page" vertical align="center">
+          <img
+            className="logo_image"
+            src="src/assets/logo.png"
+            alt="logo.png"
+          />
+          <h2>{isSignin ? "登入" : "註冊"}</h2>
+          <Form
+            className="signin_form"
+            form={form}
+            layout="vertical"
+            scrollToFirstError
+            onFinish={onFinish}
+            noValidate
+          >
+            {!isSignin && (
+              <Form.Item
+                name="name"
+                label="姓名"
+                validateTrigger="onBlur"
+                rules={[CommonRules.Required]}
+              >
+                <Input
+                  prefix={<UserOutlined />}
+                  size="large"
+                  placeholder="姓名"
+                  required
+                />
+              </Form.Item>
+            )}
             <Form.Item
-              name="name"
-              label="姓名"
+              name="email"
+              label="信箱"
               validateTrigger="onBlur"
-              rules={[CommonRules.Required]}
+              rules={[CommonRules.Email, CommonRules.Required]}
             >
               <Input
-                prefix={<UserOutlined />}
+                prefix={<MailOutlined />}
                 size="large"
-                placeholder="姓名"
+                placeholder="信箱"
                 required
               />
             </Form.Item>
-          )}
-          <Form.Item
-            name="email"
-            label="信箱"
-            validateTrigger="onBlur"
-            rules={[CommonRules.Email, CommonRules.Required]}
-          >
-            <Input
-              prefix={<MailOutlined />}
-              size="large"
-              placeholder="信箱"
-              required
-            />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            label="密碼"
-            validateTrigger="onBlur"
-            rules={[CommonRules.Required]}
-          >
-            <Input.Password
-              prefix={<LockOutlined />}
-              size="large"
-              placeholder="密碼"
-              required
-            />
-          </Form.Item>
-          {!isSignin && (
             <Form.Item
-              name="checkPassword"
-              label="再次輸入密碼"
+              name="password"
+              label="密碼"
               validateTrigger="onBlur"
-              dependencies={["password"]}
-              rules={[CommonRules.Required, CommonRules.CheckPassword]}
+              rules={[CommonRules.Required]}
             >
               <Input.Password
                 prefix={<LockOutlined />}
                 size="large"
-                placeholder="再次輸入密碼"
+                placeholder="密碼"
                 required
               />
             </Form.Item>
-          )}
-          <Form.Item>
-            <Button
-              type="primary"
-              size="large"
-              htmlType="submit"
-              style={{ width: "100%" }}
-            >
-              登入
-            </Button>
-            <Divider>或</Divider>
-            <Button
-              icon={<GoogleCircleFilled />}
-              size="large"
-              style={{ width: "100%" }}
-            >
-              Google
-            </Button>
-          </Form.Item>
-        </Form>
-        <p
-          onClick={() => {
-            setIsSignin(!isSignin);
-            form.resetFields();
-          }}
-        >
-          {isSignin ? "沒有just go 帳號了? 註冊" : "已經有just go 帳號了? 登入"}
-        </p>
+            {!isSignin && (
+              <Form.Item
+                name="checkPassword"
+                label="再次輸入密碼"
+                validateTrigger="onBlur"
+                dependencies={["password"]}
+                rules={[CommonRules.Required, CommonRules.CheckPassword]}
+              >
+                <Input.Password
+                  prefix={<LockOutlined />}
+                  size="large"
+                  placeholder="再次輸入密碼"
+                  required
+                />
+              </Form.Item>
+            )}
+            <Form.Item>
+              <Button
+                type="primary"
+                size="large"
+                htmlType="submit"
+                style={{ width: "100%" }}
+              >
+                登入
+              </Button>
+              <Divider>或</Divider>
+              <Button
+                icon={<GoogleCircleFilled />}
+                size="large"
+                style={{ width: "100%" }}
+              >
+                Google
+              </Button>
+            </Form.Item>
+          </Form>
+          <p
+            onClick={() => {
+              setIsSignin(!isSignin);
+              form.resetFields();
+            }}
+          >
+            {isSignin
+              ? "沒有just go 帳號了? 註冊"
+              : "已經有just go 帳號了? 登入"}
+          </p>
+        </Flex>
       </Flex>
     </Fragment>
   );
