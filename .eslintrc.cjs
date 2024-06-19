299418b8f53f0f3b4d809d63c2fa52d306e15263
xLanStar <danny95624268@gmail.com>
2024-06-20 01:21:11 +0800

    update packages and eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,36 +5,26 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
     "plugin:prettier/recommended",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs", "src/hooks.ts"],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "react"],
+  plugins: ["react"],
   rules: {
     "@typescript-eslint/no-restricted-imports": [
-      "warn",
+      "error",
       {
         name: "react-redux",
         importNames: ["useSelector", "useDispatch"],
+        ignorePatterns: ["src/hooks.ts"],
         message:
           "Use typed hooks `useAppDispatch` and `useAppSelector` instead.",
       },
     ],
     "no-restricted-imports": "off",
-    "prettier/prettier": [
-      "warn",
-      {},
-      {
-        usePrettierrc: true,
-      },
-    ],
+    "prettier/prettier": "error",
     "react/react-in-jsx-scope": "off",
     "react/prop-types": "off",
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
   },
   settings: {
     react: {
