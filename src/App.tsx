3805965c118f2f4d7983e811c422f29f2acd070a
flystar0526 <flowermetoer@gmail.com>
2024-06-22 10:21:28 +0800

    add trip card component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
 import Signin from "./pages/auth/Signin";
+import Dashboard from "./pages/Dashboard";
 
 const App: React.FunctionComponent = () => (
   <ConfigProvider locale={zhTW}>
@@ -13,8 +14,8 @@ const App: React.FunctionComponent = () => (
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          {/* <Route path="about" element={<About />} />
-          <Route path="dashboard" element={<Dashboard />} /> */}
+          {/* <Route path="about" element={<About />} />*/}
+          <Route path="dashboard" element={<Dashboard />} />
           <Route path="*" element={<NoMatch />} />
         </Route>
         <Route path="/signin" element={<Signin />} />
